7576be1c726177a11e1ae5d7e70704c92020b47b
Vlad-K-20 <vladislav.keller20@gmail.com>
2025-10-02 14:31:30 +0300

    Initial plugin commit: + Plugin gradle project + Ren'Py plugin icon

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.2.10"
+    id("org.jetbrains.intellij.platform") version "2.9.0"
+}
+
+group = "ee.vstepa.jetbrains.plugins"
+version = "0.0.1-b1"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+// Configure IntelliJ Platform Gradle Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+    intellijPlatform {
+        create("PC", "2025.2")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+
+        // Add necessary plugin dependencies for compilation here, example:
+        // bundledPlugin("com.intellij.java")
+    }
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+
+    // FIXME: Needed to run kotlin code during debug outside of IDE, must not be included in final build
+    implementation(kotlin("stdlib-jdk8"))
+
+    // FIXME: Needed for playing with reflection, must not be included in final build
+    implementation(kotlin("reflect"))
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "252"
+        }
+    }
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    }
+}
